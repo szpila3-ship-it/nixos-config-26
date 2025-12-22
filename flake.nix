7dc434eefea84884e9d237a4a6671c59f7113a0c
szpila3-ship-it <szpila3@gmail.com>
2025-12-22 20:40:43 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "NixOS config - draxmen";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.legion-go = nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.draxmen = import ./home-manager.nix;
+        }
+      ];
+    };
+  };
+}
